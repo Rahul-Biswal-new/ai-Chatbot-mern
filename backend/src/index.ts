185c9942c964ec28cc64075d64b2c7386f50a5b5
Rahul-Biswal-new <rahulbiswalnew@gmail.com>
2023-10-18 23:03:28 +0530

    basic setup

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+// console.log(
+//   "This is a starter kit for this amazing project. With 💓 "
+// );
+
+import express from 'express';
+const app = express();
+
+
+
+
+
+app.listen(5000, () => {
+  console.log(`running on port localhost:${5000}`);
+})
